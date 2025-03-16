9c30ef99f357817fa69ac754c84a70670e7059a9
nickperi <137534291+nickperi@users.noreply.github.com>
2025-03-15 20:28:58 -0400

    downvote.py

---
+++ App/models/downvote.py
@@ -0,0 +1,19 @@
+from App.database import db
+from sqlalchemy import ForeignKey
+
+class Downvote(db.Model):
+    __tablename__ = "downvote"
+    id = db.Column(db.Integer, primary_key=True)
+    staffid = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
+    studentid = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+
+    def __init__(self, staffid, studentid):
+        self.studentid = studentid
+        self.staffid = staffid
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'staff id': self.staffid,
+            'student id': self.studentid
+        }
