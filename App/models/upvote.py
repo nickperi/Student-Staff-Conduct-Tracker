4197f0e4bf641ea6b9c4ff2aeb14a3b182cad7ad
nickperi <137534291+nickperi@users.noreply.github.com>
2025-07-05 13:37:10 -0400

    Update upvote.py

--- App/models/upvote.py
+++ App/models/upvote.py
@@ -5,17 +5,17 @@ class Upvote(db.Model):
     __tablename__ = "upvote"
     id = db.Column(db.Integer, primary_key=True)
     staffid = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
-    studentid = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    reviewid = db.Column(db.Integer, db.ForeignKey('review.id'), nullable=False)
 
-    def __init__(self, staffid, studentid):
-        self.studentid = studentid
+    def __init__(self, staffid, reviewid):
+        self.reviewid = reviewid
         self.staffid = staffid
 
     def get_json(self):
         return {
             'id': self.id,
             'staff id': self.staffid,
-            'student id': self.studentid
+            'review id': self.reviewid
         }
 
     
