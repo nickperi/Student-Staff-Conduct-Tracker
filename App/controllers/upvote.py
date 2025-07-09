abbc440cc19768ed537aeb72e162e8f3f70cd327
nickperi <137534291+nickperi@users.noreply.github.com>
2025-07-08 23:49:53 -0400

    Update upvote.py

--- App/controllers/upvote.py
+++ App/controllers/upvote.py
@@ -4,6 +4,11 @@ from App.database import db
 def get_upvote(id):
     return Upvote.query.get(id)
 
+def get_my_upvotes(staffid):
+    my_upvotes = Upvote.query.filter_by(staffid=staffid)
+    my_upvotes = [my_upvote.reviewid for my_upvote in my_upvotes]
+    return my_upvotes
+
 def get_all_upvotes():
     return Upvote.query.all()
 
