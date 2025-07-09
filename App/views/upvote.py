4cb02e8db36dc9b1e8f33ccb6b90179e7bdcd6fc
nickperi <137534291+nickperi@users.noreply.github.com>
2025-07-08 23:48:25 -0400

    Update upvote.py

--- App/views/upvote.py
+++ App/views/upvote.py
@@ -6,6 +6,7 @@ from.index import index_views
 
 from App.controllers import (
     create_upvote,
+    remove_upvote,
     get_all_upvotes,
     get_all_upvotes_json,
     get_all_reviews,
@@ -25,17 +26,40 @@ def get_upvotes_action():
     upvotes = get_all_upvotes_json()
     return jsonify(upvotes)
 
+
 @upvote_views.route('/api/upvotes', methods=['POST'])
 @jwt_required()
 def create_upvote_endpoint():
     data = request.json
     create_upvote(jwt_current_user.id, data['reviewid'])
+    flash(f"Staff member {jwt_current_user.id} upvoted Review {data['reviewid']}")
     return jsonify({'message': f"staff {jwt_current_user.id} upvoted {data['reviewid']}"})
+
 
 @upvote_views.route('/upvotes', methods=['POST'])
 @jwt_required()
 def create_upvote_action():
-    data = request.form
-    flash(f"Staff member {jwt_current_user.id} upvoted Review {data['reviewid']}")
-    create_upvote(jwt_current_user.id, data['reviewid'])
-    return redirect(url_for('upvote_views.get_upvote_page'))
+    data = request.json
+
+    try:
+        create_upvote(jwt_current_user.id, data['reviewid'])
+        flash(f"Staff member {jwt_current_user.id} upvoted Review {data['reviewid']}")
+        redirect(url_for('upvote_views.get_upvote_page'))
+        return jsonify({'success': True, 'message': 'Upvote successful!'}), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        redirect(url_for('upvote_views.get_upvote_page'))
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+
+@upvote_views.route('/upvotes/<int:reviewid>', methods=['DELETE'])
+@jwt_required()
+def remove_upvote_action(reviewid):
+    upvote = remove_upvote(jwt_current_user.id, reviewid)
+
+    if upvote:
+        return jsonify({'success': True})
+    else:
+        return jsonify({'success': False, 'error': 'Upvote not found'}), 404
+
+
