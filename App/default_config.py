b4218406a439430342f185f4c9e62ff2e2003c7d
nickperi <137534291+nickperi@users.noreply.github.com>
2025-07-14 21:12:06 -0400

    Update default_config.py

--- App/default_config.py
+++ App/default_config.py
@@ -1,5 +1,4 @@
-#SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
-SQLALCHEMY_DATABASE_URI = "postgresql://conduct_tracker_user:UfPN9Wi6v6lWl9FAm9ElMSTUDjoKTOsx@dpg-cvb2rg52ng1s73ea1q90-a.oregon-postgres.render.com/conduct_tracker"
+SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 300
 ENV = "DEVELOPMENT"
