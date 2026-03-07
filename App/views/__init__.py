c3f6b0c2f0182bc77e51384dbb41943966046d76
nickperi <137534291+nickperi@users.noreply.github.com>
2026-03-06 23:51:31 -0400

    Add course_views to the views list

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -6,10 +6,11 @@ from .staff import staff_views
 from .review import review_views
 from .upvote import upvote_views
 from .downvote import downvote_views
+from .course import course_views
 from .index import index_views
 from .auth import auth_views
 from .admin import *
 
 
-views = [user_views, student_views, staff_views, review_views, upvote_views, downvote_views, index_views, auth_views] 
+views = [user_views, student_views, staff_views, review_views, upvote_views, downvote_views, course_views, index_views, auth_views] 
 # blueprints must be added to this list
