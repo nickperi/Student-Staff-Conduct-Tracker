4586ecde62c739206dd78538a3e568b25244785a
nickperi <137534291+nickperi@users.noreply.github.com>
2026-03-06 23:43:05 -0400

    Implement courses relationship in Student model
    Added a many-to-many relationship between Student and Course models.

--- App/models/student.py
+++ App/models/student.py
@@ -1,7 +1,13 @@
 from App.database import db
 from App.models.user import User
 from App.models.upvote import Upvote
+from App.models.course import Course
 from sqlalchemy import ForeignKey
+
+student_courses = db.Table('student_courses',
+    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+)
 
 class Student(User):
     __tablename__ = "student"
@@ -9,6 +15,7 @@ class Student(User):
     username =  db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False)
     score = db.Column(db.Integer, default=0)
+    courses = db.relationship('Course', secondary=student_courses, backref=db.backref('student', lazy='dynamic'), lazy='dynamic')
 
     __mapper_args__ = {
         'polymorphic_identity': 'student',
@@ -26,7 +33,8 @@ class Student(User):
             'id': self.id,
             'username': self.username,
             'email': self.email,
-            'score': self.score
+            'score': self.score,
+            'courses': [course.code + ' - ' + course.title for course in self.courses]
         }
     
     def get_user_type(self):
