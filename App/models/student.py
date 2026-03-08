028ff9746a01ce666aebffdc962b41f0dfdeeb98
nickperi <137534291+nickperi@users.noreply.github.com>
2026-03-08 15:16:58 -0400

    added list of students enrolled in courses page

--- App/models/student.py
+++ App/models/student.py
@@ -12,7 +12,7 @@ class Student(User):
     username =  db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False)
     score = db.Column(db.Integer, default=0)
-    courses = db.relationship('Course', secondary='student_course', backref=db.backref('student', lazy='dynamic'), lazy='dynamic')
+    courses = db.relationship('Course', secondary='student_course', backref='students')
 
     __mapper_args__ = {
         'polymorphic_identity': 'student',
